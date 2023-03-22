1ebb93738213c99de6af85dcd090607c77f5e9d3
Maxim <toper.one111@gmail.com>
2023-03-22 19:55:30 +0200

    Handler adding and removing items in Cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,7 +8,6 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    console.log(state);
     const newTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
 
@@ -39,6 +38,30 @@ const cartReducer = (state, action) => {
       totalAmount: newTotalAmount,
     };
   }
+
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
   return defaultCartState;
 };
 
