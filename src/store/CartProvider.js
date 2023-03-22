ff78024a03a960cd476f8dac841e82e15bd70936
Maxim <toper.one111@gmail.com>
2023-03-22 19:38:25 +0200

    Fix adding duplicates to the Cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,11 +8,34 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updateItems = state.items.concat(action.item);
+    console.log(state);
     const newTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItem;
+    let updatedItems;
+
+    if (existingCartItem) {
+      updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItem = { ...action.item };
+      updatedItems = state.items.concat(updatedItem);
+    }
+
     return {
-      items: updateItems,
+      items: updatedItems,
       totalAmount: newTotalAmount,
     };
   }
